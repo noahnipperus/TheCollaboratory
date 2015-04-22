7e75462f3262dcb47c4daa0082bcb82f9b43b588
EC2 Default User <ec2-user@ip-10-251-139-31.us-west-1.compute.internal>
2015-04-22 18:30:02 +0000

    ac

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 8080;
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+var todos = [];
+
+app.get("/addtodo", function (req, res) {
+	var x = req.query.newtodo;
+	todos[todos.length] = x;
+	res.end("added");
+ });
+
+
+app.get("/deletetodo", function (req, res) {
+	res.end("delete code here");
+});
+
+
+app.get("/listtodos", function (req, res) {
+	res.end(JSON.stringify(todos));
+});
+
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname + '/'));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port, hostname);
